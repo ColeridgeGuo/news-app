5dd5b00e984fc4ea4466ff310dbdcd92de53da53
Yingxuan Guo <yingxuan.guo9771@gmail.com>
2021-06-11 01:19:51 -0700

    added loading spinner and modified some styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,26 @@
 import Head from 'next/head'
-import Nav from './Nav'
+import Router from 'next/router'
+import { useEffect, useState } from 'react'
+import { BounceLoader } from 'react-spinners'
 import styles from '../styles/Layout.module.css'
+import Nav from './Nav'
 
 const Layout = ({ children }) => {
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const start = () => setLoading(true)
+    const end = () => setLoading(false)
+    Router.events.on('routeChangeStart', start)
+    Router.events.on('routeChangeComplete', end)
+    Router.events.on('routeChangeError', end)
+    return () => {
+      Router.events.off('routeChangeStart', start)
+      Router.events.off('routeChangeComplete', end)
+      Router.events.off('routeChangeError', end)
+    }
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,7 +29,13 @@ const Layout = ({ children }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Nav />
-      {children}
+      {loading && (
+        <div className={styles.spinner}>
+          <BounceLoader />
+        </div>
+      )}
+      {!loading && children}
+      <footer className={styles.footer}>Copyright 2021 Yingxuan Guo</footer>
     </div>
   )
 }
