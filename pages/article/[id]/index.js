04dd7247dae8154c473a28e5e3676ca2845c8b7e
Yingxuan Guo <yingxuan.guo9771@gmail.com>
2021-06-13 04:22:59 -0700

    set up detail news page

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,11 +1,42 @@
 import axios from 'axios'
-import articleStyles from '../../../styles/DetailNews.module.css'
+import { useState, useRef, forwardRef } from 'react'
+import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io'
+import styles from '../../../styles/DetailNews.module.css'
 
 const article = ({ article }) => {
+  const longDescp = article.descp.length > 1000
+  const [expand, setExpand] = useState(false)
+  const toggleExpand = () => {
+    setExpand(!expand)
+  }
+
   return (
-    <div>
-      <h1>{article.title}</h1>
-      <p>{article.descp}</p>
+    <div className={styles.container}>
+      <p className={styles.title}>{article.title}</p>
+      <div className={styles.info}>
+        <p>{article.date}</p>
+        <p>Icons</p>
+      </div>
+      <img src={article.image} alt="News image" className={styles.image}></img>
+      {!longDescp && <p className={styles.descp}>{article.descp}</p>}
+      {longDescp && (
+        <>
+          <p className={`${styles.descp} ${!expand ? styles.collapse : ''}`}>
+            {article.descp}
+          </p>
+          {expand ? (
+            <IoIosArrowUp
+              className={styles.expand_arrow}
+              onClick={toggleExpand}
+            />
+          ) : (
+            <IoIosArrowDown
+              className={styles.expand_arrow}
+              onClick={toggleExpand}
+            />
+          )}
+        </>
+      )}
     </div>
   )
 }
